2344dc3dddf3572df17388650ef5c248db444be7
Yanis Falaki <yanisfalaki@outlook.com>
2025-01-22 12:22:49 -0500

    removed raw_list

--- minecraft-serialization/extract_block_list.py
+++ minecraft-serialization/extract_block_list.py
@@ -1,6 +1,11 @@
 import re
 import json
 
+#
+# Raw block list is gotten from: https://piston-data.mojang.com/v1/objects/0530a206839eb1e9b35ec86acbbe394b07a2d9fb/client.txt
+# Search for net.minecraft.world.level.block.Blocks class and paste into raw_block_list.txt
+#
+
 # Read the contents of the file
 with open("raw_block_list.txt", "r") as file:
     data = file.read()
